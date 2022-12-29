ab07d1630290f1c9fc4c629936af4fe9a48163b6
Richard Feder <richardfeder@Richards-MacBook-Pro-3.local>
2022-12-29 14:06:24 -0800

    conf py bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,12 +30,18 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.autosummary',
+#     'sphinx.ext.duration',
+#     'sphinx.ext.autosummary',
+#     'nbsphinx',
+# ]
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.duration',
     'sphinx.ext.autosummary',
-    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
